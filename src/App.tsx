53be54ad6434c539dd8ac9551ca0b54e6eb58dbc
truchejude <jude.marzat@epitech.eu>
2024-07-18 17:27:57 +0200

    menu presque complet

--- src/App.tsx
+++ src/App.tsx
@@ -1,111 +1,152 @@
-import './App.css'
-import Bare from './Bare.tsx'
-import { useState, useEffect } from 'react'
-import { formatDate } from './date.tsx'
-import { menuPrincipal } from './menu.tsx'
+// App.tsx
+
+import './App.css';
+import Bare from './Bare';
+import { useState, useEffect } from 'react';
+import { formatDate } from './date';
+import { MenuItem, menuPrincipal, menuHygiène, menuNourriture, menuFrigo, menuSocial, menuÉquipements } from './menu';
 
 function App() {
-    const [money, setMoney] = useState(() => {
-      const savedMoney = localStorage.getItem('money');
-      return savedMoney !== null ? Number(savedMoney) : 100;
-    });
-    const [hp, setHp] = useState(() => {
-      const savedHp = localStorage.getItem('hp');
-      return savedHp !== null ? Number(savedHp) : 100;
-    });
-    const [food, setFood] = useState(() => {
-      const savedFood = localStorage.getItem('food');
-      return savedFood !== null ? Number(savedFood) : 100;
-    });
-    const [joie, setJoie] = useState(100)
-    const [menuSelect, setMenuSelect] = useState(menuPrincipal)
-    const [menuIndex, setMenuIndex] = useState(0)
-    
-    const intervalTime = 500;
+  const [money, setMoney] = useState<number>(() => {
+    const savedMoney = localStorage.getItem('money');
+    return savedMoney !== null ? Number(savedMoney) : 100;
+  });
+  const [hp, setHp] = useState<number>(() => {
+    const savedHp = localStorage.getItem('hp');
+    return savedHp !== null ? Number(savedHp) : 100;
+  });
+  const [food, setFood] = useState<number>(() => {
+    const savedFood = localStorage.getItem('food');
+    return savedFood !== null ? Number(savedFood) : 100;
+  });
+  const [menuSelect, setMenuSelect] = useState<MenuItem[]>(menuPrincipal);
+  const [menuIndex, setMenuIndex] = useState<number>(0);
 
-    function setFoodAnLocal(value: number) {
-      setFood(value);
-      localStorage.setItem('food', value.toString());
-    }
+  const intervalTime = 500;
 
-    function setMoulaLocal(value: number) {
-      setMoney(value);
-      localStorage.setItem('money', value.toString());
-    }
-
-    function setHpLocal(value: number) {
-      setHp(value);
-      localStorage.setItem('hp', value.toString());
-    }
-
-    useEffect(() => {
-      const lastUpdateTime = localStorage.getItem('lastUpdateTime');
-      const now = Date.now();
-
-      if (lastUpdateTime) {
-        const elapsedTime = now - Number(lastUpdateTime);
-        const timesDecremented = Math.floor(elapsedTime / intervalTime);
-        if (timesDecremented > 0) {
-          const newFoodValue = Math.max(food - timesDecremented, 0);
-          setFoodAnLocal(newFoodValue);
-        }
-      }
-      localStorage.setItem('lastUpdateTime', now.toString());
-
-      const interval = setInterval(() => {
-        setFood((prevFood) => {
-          const newFoodValue = Math.max(prevFood - 1, 0);
-          localStorage.setItem('food', newFoodValue.toString());
-          localStorage.setItem('lastUpdateTime', Date.now().toString());
-          if (newFoodValue !== 0) {
-            localStorage.setItem('prochaineFamine', (Date.now() + (newFoodValue * intervalTime)).toString());
-            console.log("new food value: ", formatDate(Date.now() + (newFoodValue * intervalTime)))
-          } else if (newFoodValue === 0 && hp > 0) {
-            setHpLocal(Math.max(hp - 1, 0))
-            console.log("hp", hp)
-          }
-          return newFoodValue;
-        });
-      }, intervalTime);
-
-      return () => clearInterval(interval);
-    }, [hp]);
-
-    useEffect(() => {
-      const now = Date.now();
-      const prochaineFamine = Number(localStorage.getItem('prochaineFamine'));
-
-      console.log(formatDate(prochaineFamine), (now - prochaineFamine) / intervalTime)
-      if (((now - prochaineFamine) / intervalTime) > 0) {
-        console.log("cacac", (now - prochaineFamine) / intervalTime, Math.max(hp - ((now - prochaineFamine) / intervalTime), 0))
-        setHpLocal(Math.max(hp - ((now - prochaineFamine) / intervalTime), 0))
-      }
-    }, [])
-
-    return (
-      <div className='contenant'>
-        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
-          <Bare value={hp} valueMax={100} name="HP" />
-          <Bare value={food} valueMax={100} name="Food" />
-          <Bare value={joie} valueMax={100} name="Happy" />
-          <div style={{ display: 'flex' }}><p><strong>Moula: {money}</strong></p></div>
-        </div>
-        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
-          <button onClick={() => setHpLocal(Math.min(hp + 10, 100))}>Heal</button>
-          <button onClick={() => setFoodAnLocal(Math.min(food + 10, 100))}>Feed</button>
-          <button onClick={() => setMoulaLocal(money + 100)}>Branle</button>
-        </div>
-        <div className='game'>
-          <div className='menu'>
-            <p>{menuPrincipal[menuIndex].name}</p>
-          </div>
-        </div>
-        <button onClick={() => setMenuIndex((menuIndex + 1) >= menuSelect.length ? 0 : menuIndex + 1)}>A</button>
-        <button onClick={() => console.log("B")}>B</button>
-        <button onClick={() => console.log("C")}>C</button>
-        <button onClick={() => console.log("D")}>D</button>
-      </div>
-    )
+  function setFoodAnLocal(value: number) {
+    setFood(value);
+    localStorage.setItem('food', value.toString());
   }
 
-export default App
+  function setMoulaLocal(value: number) {
+    setMoney(value);
+    localStorage.setItem('money', value.toString());
+  }
+
+  function setHpLocal(value: number) {
+    setHp(value);
+    localStorage.setItem('hp', value.toString());
+  }
+
+  useEffect(() => {
+    const lastUpdateTime = localStorage.getItem('lastUpdateTime');
+    const now = Date.now();
+
+    if (lastUpdateTime) {
+      const elapsedTime = now - Number(lastUpdateTime);
+      const timesDecremented = Math.floor(elapsedTime / intervalTime);
+      if (timesDecremented > 0) {
+        const newFoodValue = Math.max(food - timesDecremented, 0);
+        setFoodAnLocal(newFoodValue);
+      }
+    }
+    localStorage.setItem('lastUpdateTime', now.toString());
+
+    const interval = setInterval(() => {
+      setFood((prevFood) => {
+        const newFoodValue = Math.max(prevFood - 1, 0);
+        localStorage.setItem('food', newFoodValue.toString());
+        localStorage.setItem('lastUpdateTime', Date.now().toString());
+        if (newFoodValue !== 0) {
+          localStorage.setItem('prochaineFamine', (Date.now() + (newFoodValue * intervalTime)).toString());
+          console.log("new food value: ", formatDate(Date.now() + (newFoodValue * intervalTime)));
+        } else if (newFoodValue === 0 && hp > 0) {
+          setHpLocal(Math.max(hp - 1, 0));
+          console.log("hp", hp);
+        }
+        return newFoodValue;
+      });
+    }, intervalTime);
+
+    return () => clearInterval(interval);
+  }, [hp]);
+
+  useEffect(() => {
+    const now = Date.now();
+    const prochaineFamine = Number(localStorage.getItem('prochaineFamine'));
+
+    console.log(formatDate(prochaineFamine), (now - prochaineFamine) / intervalTime);
+    if (((now - prochaineFamine) / intervalTime) > 0) {
+      console.log("cacac", (now - prochaineFamine) / intervalTime, Math.max(hp - ((now - prochaineFamine) / intervalTime), 0));
+      setHpLocal(Math.max(hp - ((now - prochaineFamine) / intervalTime), 0));
+    }
+  }, []);
+
+  const handleAction = (action: MenuItem) => {
+    switch (action.type) {
+      case 'goToMenu':
+        switch (action.name) {
+          case 'hygiène':
+            setMenuSelect(menuHygiène);
+            break;
+          case 'nourriture':
+            setMenuSelect(menuNourriture);
+            break;
+          case 'plaisir':
+            // Ajoutez le menu plaisir ici quand vous le créez
+            break;
+          case 'social':
+            setMenuSelect(menuSocial);
+            break;
+          case 'équipements':
+            setMenuSelect(menuÉquipements);
+            break;
+          case 'frigo':
+            setMenuSelect(menuFrigo);
+            break;
+          default:
+            setMenuSelect(menuPrincipal);
+            break;
+        }
+        setMenuIndex(0);
+        break;
+      case 'action':
+        // Exécutez des actions spécifiques ici
+        console.log(`Action exécutée: ${action.name}`);
+        break;
+      case 'objetAction':
+        // Exécutez des actions spécifiques liées à un objet ici
+        console.log(`Action objet exécutée: ${action.name} avec les données ${action.data}`);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className='contenant'>
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
+        <Bare value={hp} valueMax={100} name="HP" />
+        <Bare value={food} valueMax={100} name="Food" />
+        <div style={{ display: 'flex' }}><p><strong>Moula: {money}</strong></p></div>
+      </div>
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
+        <button onClick={() => setHpLocal(Math.min(hp + 10, 100))}>Heal</button>
+        <button onClick={() => setFoodAnLocal(Math.min(food + 10, 100))}>Feed</button>
+        <button onClick={() => setMoulaLocal(money + 100)}>Branle</button>
+      </div>
+      <div className='game'>
+        <div className='menu'>
+          <p>{menuSelect[menuIndex].name}</p>
+        </div>
+      </div>
+      <button onClick={() => setMenuIndex((menuIndex + 1) >= menuSelect.length ? 0 : menuIndex + 1)}>A</button>
+      <button onClick={() => handleAction(menuSelect[menuIndex])}>B</button>
+      <button onClick={() => setMenuSelect(menuPrincipal)}>C</button>
+      <button onClick={() => console.log("D")}>D</button>
+    </div>
+  );
+}
+
+export default App;
--- src/menu.tsx
+++ src/menu.tsx
@@ -5,7 +5,16 @@
     objetAction -> exécute une action en fonction de l'objet sélectionné dans le tableau stocké dans le local storage data${TRUC}
 */
 
-export const menuPrincipal = [
+export type MenuType = 'goToMenu' | 'action' | 'objetAction';
+
+export interface MenuItem {
+    name: string;
+    id: number;
+    type: MenuType;
+    data?: string;
+}
+
+export const menuPrincipal: MenuItem[] = [
     { name: "hygiène", id: 1, type: "goToMenu"},
     { name: "nourriture", id: 2, type: "goToMenu"},
     { name: "plaisir", id: 3, type: "goToMenu"},
@@ -13,32 +22,32 @@ export const menuPrincipal = [
     { name: "équipements", id: 15, type: "goToMenu"}
 ]
 
-export const menuHygiène = [
+export const menuHygiène: MenuItem[] = [
     { name: "douche", id: 4, type: "action"},
     { name: "médicament", id: 5, type: "action"},
     { name: "dodo", id: 6, type: "action"},
     { name: "nettoyer la chambre", id: 7, type: "action"}
 ]
 
-export const menuNourriture = [
+export const menuNourriture: MenuItem[] = [
     { name: "frigo", id: 8, type: "goToMenu"},
     { name: "acheter nourriture", id: 9, type: "goToMenu"},
 ]
 
-export const menuFrigo = [
+export const menuFrigo: MenuItem[] = [
     { name: "jeter nourriture", id: 10, type: "objetAction", data: "dataFrigo"},
     { name: "donner nourriture", id: 11, type: "objetAction", data: "dataFrigo"},
 ]
 
-export const menuSocial = [
+export const menuSocial: MenuItem[] = [
     { name: "parler à un ami", id: 22, type: "objetAction", data: "dataAmis"},
     { name: "aller à une fête", id: 23, type: "action"},
     { name: "faire du sex", id: 24, type: "objetAction", data: "dataAmis"}
 ]
 
-export const menuÉquipements = [
-    { name: "acheter des vêtements", id: 25, type: "action"},
-    { name: "acheter des meubles", id: 26, type: "action"},
-    { name: "équiper des vêtements", id: 25, type: "objetAction", data: "vêtements"},
-    { name: "posser des meubles", id: 26, type: "objetAction", data: "meubles"},
+export const menuÉquipements: MenuItem[] = [
+    { name: "acheter des vêtements", id: 25, type: "objetAction", data: "dataAcheterVêtements"},
+    { name: "acheter des meubles", id: 26, type: "objetAction", data: "dataAcheterMeubles"},
+    { name: "équiper des vêtements", id: 25, type: "objetAction", data: "dataVêtements"},
+    { name: "posser des meubles", id: 26, type: "objetAction", data: "dataMeubles"},
 ]
